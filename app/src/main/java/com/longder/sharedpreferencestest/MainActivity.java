ef670f8bd3c017de442d405471808e7b627d351e
Longder1989 <longder1989@gmail.com>
2016-05-30 16:39:38 +0800

    加了一些比较详尽的注释

--- app/src/main/java/com/longder/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/longder/sharedpreferencestest/MainActivity.java
@@ -18,26 +18,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         saveButton = (Button) findViewById(R.id.save_data);
         restoreButton = (Button) findViewById(R.id.restore_data);
+        /**
+         * 数据写入
+         */
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /**
-                 * 数据写入
-                 */
+
+                //得到SharedPreferences.Editor对象，这个对象对应的文件名是"data"，Context.MODE_PRIVATE指定读写权限
                 SharedPreferences.Editor editor = getSharedPreferences("data", Context.MODE_PRIVATE).edit();
+                /**
+                 * 以下三行代码用来存储数据，editor提供putXXX方法以键值对的形式存数
+                 */
                 editor.putInt("age", 18);
                 editor.putString("name", "大表哥");
                 editor.putBoolean("married", false);
-                editor.commit();
+                //提交持久化
+                editor.apply();
             }
         });
+        /**
+         * 数据读取
+         */
         restoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /**
-                 * 数据读取
-                 */
+                //获取SharedPreferences对象，通过指定文件名"data"和读取模式Context.MODE_PRIVATE
                 SharedPreferences sp = getSharedPreferences("data", Context.MODE_PRIVATE);
+                /**
+                 * 以下三行代码用来获取SharedPreferences中存储的数据，使用getXXX
+                 * 第一个参数是key，后一个参数是默认值。当根据这个key找不到数据时返回。
+                 */
                 String name = sp.getString("name", "");
                 int age = sp.getInt("age", 0);
                 boolean married = sp.getBoolean("married", true);
